72a09053c10e8d9817f8ea979430981022d97a9a
Tilaknaik01 <tilaknaik@yagoo.com>
2023-06-02 16:09:37 +0530

    commotwertyu

---
+++ src/test/java/tilak/Pgm1.java
@@ -0,0 +1,12 @@
+package tilak;
+
+import org.testng.annotations.Test;
+
+public class Pgm1 
+{
+	@Test
+	public void test1() {
+		// TODO Auto-generated method stub
+		System.out.println("p1");
+	}
+}
